d1cc6a2975ea093dc224e5e269b482c3b26a0651
seokbangguri <pique99@naver.com>
2023-09-14 17:55:56 +0900

    add gradient-green

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div className="gradient-green">Learn React</div>
+          Learn React
         </a>
       </header>
     </div>
