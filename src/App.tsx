cf74aa4fedc2b0f6f988a52d2ef9949508e783e0
seokbangguri <pique99@naver.com>
2023-09-15 14:10:20 +0900

    fix card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Footer, Header } from "./components";
+import { Footer, Header, Card } from "./components";
 import Heading from "./components/Heading/Heading";
-import Card from "./components/Card/Card"
 
 function App() {
   return (
@@ -13,20 +12,27 @@ function App() {
         </Heading>
       </main>
       <Footer />
-      <Card
-        source="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
-        name="card"
-        explain={["explain1 1-1","explain2 1-2","explain3 1-3", "test 1-4"]}
-      >
-          card test
-      </Card>
-      <Card
-        source="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
-        name="card"
-        explain={["explain1 2-1","explain2 2-2","explain3 2-3", "test 2-4"]}
-      >
-          card test
-      </Card>
+      <section>
+        <Heading tag="h2">
+          Our Products and Services
+        </Heading>
+        <div className="flex">
+          <Card
+            source="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+            name="card"
+            explain={["explain1 1-1","explain2 1-2","explain3 1-3", "test 1-4"]}
+          >
+              card test
+          </Card>
+          <Card
+            source="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+            name="card"
+            explain={["explain1 2-1","explain2 2-2","explain3 2-3", "test 2-4"]}
+          >
+              card test
+          </Card>
+        </div>
+      </section>
     </div>
   );
 }
