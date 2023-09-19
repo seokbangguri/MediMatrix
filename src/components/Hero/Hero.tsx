8c0a72016e7f8440908af5669166d9a1cc5495d9
seokbangguri <pique99@naver.com>
2023-09-19 13:30:01 +0900

    translate to korean

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import Heading from "../Heading/Heading";
 import Text from "../Text/Text";
 import Button from "../Button/Button";
+import "../../../src/index.css";
+
 
 const Hero = () => {
   return (
     <section className="px-5 lg:px-10 flex flex-col justify-center items-center py-20 h-screen">
       <Heading tag="h1" className="">
-        Where teams and work <br /> come together
+        MediMatrix는 AI 기반 알고리즘을 통해 <br/>자동적이고 객관적인 서비스를 제공합니다.
       </Heading>
       <Text size="l" styles="max-w-[450px] text-center mb-16">
-        Bring everyone's work into one place: tasks, docs, whteboard, chats,
-        goals, and more.
+        Beery VMI, example, example, example
       </Text>
-      <Button apperance="primary">Get Started. It's FREE</Button>
+      <div id="container">
+        <div id="chevron"></div>
+        <div id="chevron"></div>
+        <div id="chevron"></div>
+      </div>
+      <Button apperance="primary">자세히 알아보기</Button>
     </section>
   );
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -15,33 +15,33 @@ const SignUp = () => {
   <div className="w-[500px] bg-white rounded-lg drop-shadow-xl ">
         <div className="p-10 space-y-4 md:space-y-6 sm:p-8">
             <Heading tag='h3' className="text-center">
-                Register
+                회원가입
             </Heading>
             <form className="space-y-4 md:space-y-6" action="#">
                 <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
                     <input type="email" name="email" id="email" className="bg-stone-100 border border-gray-300 text-gray-900 sm:text-sm rounded-xs focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " placeholder="name@company.com" required/>
                 </div>
                 <div>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-slate-900 ">Password</label>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-slate-900 ">비밀번호</label>
                     <input type="password" name="password" id="password" placeholder="••••••••" className="bg-stone-100 border border-gray-300  sm:text-sm rounded-xs focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required/>
                 </div>
                 <div>
-                    <label htmlFor="confirmPassor" className="block mb-2 text-sm font-medium text-slate-900 ">Confirm Password</label>
+                    <label htmlFor="confirmPassor" className="block mb-2 text-sm font-medium text-slate-900 ">비밀번호 확인</label>
                     <input type="password" name="confirmPassor" id="confirmPassor" placeholder="••••••••" className="bg-stone-100 border border-gray-300  sm:text-sm rounded-xs focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required/>
                 </div>
                 <div>
-                    <label htmlFor="hospital" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hospital Name</label>
+                    <label htmlFor="hospital" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">병원 이름</label>
                     <input type="text" name="hospital" id="hospital" className="bg-stone-100 border border-gray-300 text-gray-900 sm:text-sm rounded-xs focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " placeholder="Enter hospital name..." required/>
                 </div>
                 <div>
-                    <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone</label>
+                    <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">전화번호</label>
                     <input type="tel" name="phone" id="phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" className="bg-stone-100 border border-gray-300 text-gray-900 sm:text-sm rounded-xs focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " placeholder="Enter phone number..."/>
                 </div>
                 
-                <Button apperance="primary" type="submit" styles="w-full text-center">Sign Up</Button>
+                <Button apperance="primary" type="submit" styles="w-full text-center">회원가입</Button>
                 <p className="text-sm font-light text-[#7a7a7a]">
-                    Do have an account? <a href="/signin" className="font-medium text-blue-600 hover:underline "> Sign In</a>
+                    계정이 있으십니까? <a href="/signin" className="font-medium text-blue-600 hover:underline "> 로그인</a>
                 </p>
             </form>
         </div>
