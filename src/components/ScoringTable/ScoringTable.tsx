dbc7ae85a2b9e0828ed37166d21feb1fc3ae251f
Usmon <usmonhakimov1999@gmail.com>
2023-09-20 11:02:42 +0900

    add firebase

--- src/components/ScoringTable/ScoringTable.tsx
+++ src/components/ScoringTable/ScoringTable.tsx
@@ -1,37 +1,47 @@
 import React from 'react'
-import {c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30} from '../../assets';
+import { c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30 } from '../../assets';
 
 
 // const cImageArray = [c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20,c21,c22,c23,c24,c25,c26,c27,c28,c29,c30,]
 
 const ScoringTable = () => {
-    const data = [
-        {
-          index: '7번',
-          image: c7,
-          text: '1. 선의 1/2 이상이 수직선으로부터 30도 이상 벗어나지 않을 것',
-        },
-        {
-          index: '8번',
-          image: c8,
-          text: '1. 선의 1/2 이상이 수직선으로부터 30도 이상 벗어나지 않을 것',
-        },
-        {
-          index: '9번',
-          image: c9,
-          text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것',
-        },
-        {
-          index: '10번',
-          image: c10,
-          text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것',
-        },
-        {
-          index: '11번',
-          image: c11,
-          text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것.',
-        },
-      ];
+  const data = [
+    {
+      index: '7번',
+      image: c7,
+      text: '1. 선의 1/2 이상이 수직선으로부터 30도 이상 벗어나지 않을 것',
+    },
+    {
+      index: '8번',
+      image: c8,
+      text: '1. 선의 1/2 이상이 수직선으로부터 30도 이상 벗어나지 않을 것',
+    },
+    {
+      index: '9번',
+      image: c9,
+      text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것',
+    },
+    {
+      index: '10번',
+      image: c10,
+      text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것',
+    },
+    {
+      index: '11번',
+      image: c11,
+      text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것.',
+    },
+    {
+      index: '12번',
+      image: c12,
+      text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것',
+    },
+    {
+      index: '13번',
+      image: c13,
+      text: '1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것. 1. 고리의 높이(height)의 폭(width) 비율이 2:1 이내일 것.',
+    },
+  ];
   return (
     <div className="w-[800px] bg-white rounded-lg p-4">
       <h4 className='p-4 pb-6 text-xl font-semibold'>채점 규칙</h4>
@@ -47,11 +57,11 @@ const ScoringTable = () => {
               <td className='w-2/12 p-2 text-center flex flex-col items-center justify-center'>
                 <span className='text-sm mb-1'>1점 예시</span>
                 <img src={item.image} alt={`example ${item.index}`} className="rounded-sm w-[95px]" />
-                </td>
+              </td>
               <td className='w-2/12 p-2 text-center flex flex-col items-center justify-center'>
-              <span className='text-sm mb-1'>0점 예시</span>
+                <span className='text-sm mb-1'>0점 예시</span>
                 <img src={item.image} alt={`example ${item.index}`} className="rounded-sm w-[95px]" />
-                </td>
+              </td>
             </tr>
           ))}
         </tbody>
