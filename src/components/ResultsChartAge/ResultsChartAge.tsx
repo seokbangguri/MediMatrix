3a44360ab56b2c6bacd68ff645ce68c85d137225
Usmon <usmonhakimov1999@gmail.com>
2023-09-20 09:46:53 +0900

    add scoring rules section

--- src/components/ResultsChartAge/ResultsChartAge.tsx
+++ src/components/ResultsChartAge/ResultsChartAge.tsx
@@ -3,113 +3,124 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Resp
 import Heading from '../Heading/Heading';
 
 const data = [
-    {
-      name: '2세',
-      uv: 6,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '3세',
-      uv: 11,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '4세',
-      uv: 14,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '5세',
-      uv: 17,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '6세',
-      uv: 19,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '7세',
-      uv: 20,
-      pv: 100,
-      amt: 100,
-    },
-    {
-      name: '8세',
-      uv: 20.5,
-      pv: 100,
-      amt: 100,
-    },
-    {
-        name: '9세',
-        uv: 21,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '10세',
-        uv: 21.3,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '11세',
-        uv: 21.6,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '12세',
-        uv: 22,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '15세',
-        uv: 22.5,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '16세',
-        uv: 23,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '17세',
-        uv: 24,
-        pv: 100,
-        amt: 100,
-      },
-      {
-        name: '18세 이상',
-        uv: 25,
-        pv: 100,
-        amt: 100,
-      },
-  ];
+  {
+    name: '2세',
+    uv: 6,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '3세',
+    uv: 11,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '4세',
+    uv: 14,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '5세',
+    uv: 17,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '6세',
+    uv: 19,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '7세',
+    uv: 20,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '8세',
+    uv: 20.5,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '9세',
+    uv: 21,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '10세',
+    uv: 21.3,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '11세',
+    uv: 21.6,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '12세',
+    uv: 22,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '15세',
+    uv: 22.5,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '16세',
+    uv: 23,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '17세',
+    uv: 24,
+    pv: 100,
+    amt: 100,
+  },
+  {
+    name: '18세 이상',
+    uv: 25,
+    pv: 100,
+    amt: 100,
+  },
+];
 const ResultsChartAge = () => {
   return (
     <div className='w-[600px] h-[300px] bg-white p-4 rounded-md'>
-        <h4 className='p-4 pb-6 text-sm font-semibold'>연령대별 채점 결과 점수 통계</h4>
-        <div className='w-full h-[200px]'>
-            <ResponsiveContainer  width="100%" height="100%" >
-                <BarChart width={200} height={40} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis type="number" domain={[0, 30]} />
-                    <Tooltip />
-                    <Bar dataKey="uv" fill="#3FB6D3" />
-                </BarChart>
-            </ResponsiveContainer>
-        </div>
-        
-        
+      <h4 className='p-4 pb-6 text-sm font-semibold'>연령대별 채점 결과 점수 통계</h4>
+      <div className='w-full h-[200px]'>
+        <ResponsiveContainer width="100%" height="100%" >
+          <BarChart width={200} height={40} data={data}>
+            <XAxis dataKey="name" />
+            <YAxis type="number" domain={[0, 30]} />
+            <Tooltip />
+            <Bar dataKey="uv" fill="#3FB6D3" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+      <h4 className='p-4 text-sm font-semibold'>채점시 유의사항</h4>
+      <ul className='text-sm max-w-[480px] list-disc w-full ml-10 list-inside space-y-2'>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolore at expedita nam consequatur voluptatum quaerat blanditiis incidunt fugit dolor?</li>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolore at expedita nam consequatur voluptatum</li>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      </ul>
+      <h4 className='p-4 text-sm font-semibold'>총점 산정 방식</h4>
+      <ul className='text-sm max-w-[480px] list-disc w-full ml-10 list-inside space-y-2'>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolore at expedita nam consequatur voluptatum quaerat blanditiis incidunt fugit dolor?</li>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolore at expedita nam consequatur voluptatum</li>
+        <li className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      </ul>
+
     </div>
   )
 }
