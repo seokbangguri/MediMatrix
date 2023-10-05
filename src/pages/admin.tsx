9675a1e3a60c4b8235391a8a92a5babc17855e5b
Usmon <usmonhakimov1999@gmail.com>
2023-10-05 13:15:51 +0900

    update admin page, add patients section

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -16,30 +16,64 @@ const Admin = () => {
                     <span className="font-bold text-[20px] w-[150px]">Name</span>
                     <span className="font-bold text-[20px] w-[150px]">Email</span>
                     <span className="font-bold text-[20px] w-[150px]">Phone Number</span>
-                    <span className="font-bold text-[20px] w-[50px] text-center">Patients</span>
+                    <span className="font-bold text-[20px] w-[150px] text-center">Patients</span>
                     <span className="font-bold text-[20px] w-[150px] text-center">See Patients</span>
                     <span className="font-bold text-[20px] w-[100px] text-center">Delete</span>
                 </div>
                 <div className="">
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
+                <ListItem name='Kim' id='kim@gmail.com' type='23' score='01028872323'/>
+                <ListItem name='Min' id='min@gmail.com' type='23' score='01028872323'/>
+                <ListItem name='Wang' id='kim@gmail.com' type='28' score='01028872323'/>
+                <ListItem name='Wang' id='kim@gmail.com' type='28' score='01028872323'/>
+                <ListItem name='Wang' id='kim@gmail.com' type='28' score='01028872323'/>
+                <ListItem name='Wang' id='kim@gmail.com' type='28' score='01028872323'/>
+                <ListItem name='Wang' id='kim@gmail.com' type='28' score='01028872323'/>
                 </div>
-
-
             </div>
         </div>
+        <Patients name='Usmon' id='1' score="45" type="Berry VMI" />
     </div>;
 };
 
 export default Admin;
 
 
-export const ListItem = () => {
+
+interface patientsProps {
+    name: string,
+    id: string,
+    score?: string,
+    type?: string,
+}
+
+export const Patients = ({name, id, score, type}: patientsProps) => {
+
+    return (
+        <div className="w-full self-start mt-16">
+            <Heading tag="h3" className="tex-left">Therapist Kim's patients</Heading>
+            <div className="w-full bg-white rounded-sm drop-shadow-2xl p-4 mt-8">
+                <div className="w-full flex justify-between gap-2 border-b border-[#cecece] py-4">
+                    <span className="font-bold text-[20px] w-[30px]">#</span>
+                    <span className="font-bold text-[20px] w-[150px]">Name</span>
+                    <span className="font-bold text-[20px] w-[150px]">ID</span>
+                    <span className="font-bold text-[20px] w-[150px]">Score</span>
+                    <span className="font-bold text-[20px] w-[150px] text-center">Type</span>
+                    <span className="font-bold text-[20px] w-[150px] text-center">See Results</span>
+                    <span className="font-bold text-[20px] w-[100px] text-center">Remove</span>
+                </div>
+                <div className="">
+                    <ListItem name={name} id={id} type={type} score={score}/>
+                    <ListItem name={name} id={id} type={type} score={score}/>
+                    <ListItem name={name} id={id} type={type} score={score}/>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+
+export const ListItem = (props: patientsProps) => {
     const showAlert = () => {
         Swal.fire({
             title: 'Are you sure?',
@@ -62,11 +96,11 @@ export const ListItem = () => {
     }
     return (
         <div className="w-full flex justify-between text-left  gap-2 py-2">
-            <span className="text-[20px] w-[30px] ">1</span>
-            <span className="text-[20px] w-[150px] ">Usmon</span>
-            <span className="text-[20px] w-[150px]">usmon@gmail.com</span>
-            <span className="text-[20px] w-[150px]">Phone Number</span>
-            <span className="text-[20px] w-[50px] text-center">20</span>
+            <span className="text-base w-[30px] ">1</span>
+            <span className="text-base w-[150px] ">{props.name}</span>
+            <span className="text-base w-[150px]">{props.id}</span>
+            <span className="text-base w-[150px]">{props.score}</span>
+            <span className="text-base w-[150px] text-center">{props.type}</span>
             <span className="text-[20px] w-[150px] flex items-start justify-center"><button type="button"><RiShareBoxFill className="w-[50px]" /></button></span>
             <span className="text-[20px] w-[100px] flex items-start justify-center text-red-600"><button type="button" onClick={showAlert}><HiOutlineUserRemove className="w-[50px]" /></button></span>
         </div>
