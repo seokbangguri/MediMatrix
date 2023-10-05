9ca5c9556a0c1be16ffa5b978e088dfb2f1a76ad
seokbangguri <pique99@naver.com>
2023-10-05 11:11:48 +0900

    catch export error

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise"); // 수정: mysql2/promise 모듈 사용
