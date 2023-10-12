c56d28ff287ae9971e0a55c4471374c471529d06
Ubuntu <vmadmin@service-vm.qrj225xhquuujklttntkcnay3b.syx.internal.cloudapp.net>
2023-10-12 07:39:36 +0000

    t

--- api/route.js
+++ api/route.js
@@ -8,7 +8,7 @@ router.post("/setting", loadUserData);
 router.post("/updatedata", updateData);
 router.post("/updatepw", updatePassword);
 
-app.listen(port, () => {
+router.listen(port, () => {
     console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
   });
   
