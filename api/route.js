77c2ee707849fce1a87d288d3c04a760da5fd9b2
seokbangguri <pique99@naver.com>
2023-10-19 17:18:00 +0900

    modify route.js

--- api/route.js
+++ api/route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, loadUserData, updateData, updatePassword, patientE, verifyToken } = require("./controllers/userController");
+const { signup, signin, loadUserData, updateData, updatePassword, patientE, patientL, verifyToken } = require("./controllers/userController");
 
 router.post("/signup", signup);
 router.post("/signin", signin);
