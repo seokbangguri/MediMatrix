b3438dba84b2b37be78081b0a083b7e1ef156815
Usmon <usmonhakimov1999@gmail.com>
2023-10-20 10:55:22 +0900

    uninstall recharts package

--- src/components/ResultsChartAge/ResultsChartAge.tsx
+++ src/components/ResultsChartAge/ResultsChartAge.tsx
@@ -1,112 +1,169 @@
-
-// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Heading from '../Heading/Heading';
+import { ResponsivePie } from '@nivo/pie';
 
 const data = [
   {
-    name: '2세',
-    uv: 6,
-    pv: 100,
-    amt: 100,
+    "id": "elixir",
+    "label": "elixir",
+    "value": 153,
+    "color": "hsl(275, 70%, 50%)"
   },
   {
-    name: '3세',
-    uv: 11,
-    pv: 100,
-    amt: 100,
+    "id": "css",
+    "label": "css",
+    "value": 586,
+    "color": "hsl(244, 70%, 50%)"
   },
   {
-    name: '4세',
-    uv: 14,
-    pv: 100,
-    amt: 100,
+    "id": "php",
+    "label": "php",
+    "value": 413,
+    "color": "hsl(327, 70%, 50%)"
   },
   {
-    name: '5세',
-    uv: 17,
-    pv: 100,
-    amt: 100,
+    "id": "javascript",
+    "label": "javascript",
+    "value": 94,
+    "color": "hsl(130, 70%, 50%)"
   },
   {
-    name: '6세',
-    uv: 19,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '7세',
-    uv: 20,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '8세',
-    uv: 20.5,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '9세',
-    uv: 21,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '10세',
-    uv: 21.3,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '11세',
-    uv: 21.6,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '12세',
-    uv: 22,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '15세',
-    uv: 22.5,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '16세',
-    uv: 23,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '17세',
-    uv: 24,
-    pv: 100,
-    amt: 100,
-  },
-  {
-    name: '18세 이상',
-    uv: 25,
-    pv: 100,
-    amt: 100,
-  },
-];
+    "id": "make",
+    "label": "make",
+    "value": 566,
+    "color": "hsl(182, 70%, 50%)"
+  }
+]
 const ResultsChartAge = () => {
   return (
     <div className='w-[600px] h-[300px] bg-white p-4 rounded-md'>
       <h4 className='p-4 pb-6 text-sm font-semibold'>연령대별 채점 결과 점수 통계</h4>
-      <div className='w-full h-[200px]'>
-        {/* <ResponsiveContainer width="100%" height="100%" >
-          <BarChart width={200} height={40} data={data}>
-            <XAxis dataKey="name" />
-            <YAxis type="number" domain={[0, 30]} />
-            <Tooltip />
-            <Bar dataKey="uv" fill="#3FB6D3" />
-          </BarChart>
-        </ResponsiveContainer> */}
+      <div className='w-full h-[400px]'>
+        <ResponsivePie
+          data={data}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{
+            from: 'color',
+            modifiers: [
+              [
+                'darker',
+                0.2
+              ]
+            ]
+          }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: 'color' }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{
+            from: 'color',
+            modifiers: [
+              [
+                'darker',
+                2
+              ]
+            ]
+          }}
+          defs={[
+            {
+              id: 'dots',
+              type: 'patternDots',
+              background: 'inherit',
+              color: 'rgba(255, 255, 255, 0.3)',
+              size: 4,
+              padding: 1,
+              stagger: true
+            },
+            {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'inherit',
+              color: 'rgba(255, 255, 255, 0.3)',
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10
+            }
+          ]}
+          fill={[
+            {
+              match: {
+                id: 'ruby'
+              },
+              id: 'dots'
+            },
+            {
+              match: {
+                id: 'c'
+              },
+              id: 'dots'
+            },
+            {
+              match: {
+                id: 'go'
+              },
+              id: 'dots'
+            },
+            {
+              match: {
+                id: 'python'
+              },
+              id: 'dots'
+            },
+            {
+              match: {
+                id: 'scala'
+              },
+              id: 'lines'
+            },
+            {
+              match: {
+                id: 'lisp'
+              },
+              id: 'lines'
+            },
+            {
+              match: {
+                id: 'elixir'
+              },
+              id: 'lines'
+            },
+            {
+              match: {
+                id: 'javascript'
+              },
+              id: 'lines'
+            }
+          ]}
+          legends={[
+            {
+              anchor: 'bottom',
+              direction: 'row',
+              justify: false,
+              translateX: 0,
+              translateY: 56,
+              itemsSpacing: 0,
+              itemWidth: 100,
+              itemHeight: 18,
+              itemTextColor: '#999',
+              itemDirection: 'left-to-right',
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: 'circle',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemTextColor: '#000'
+                  }
+                }
+              ]
+            }
+          ]}
+        />
       </div>
       <h4 className='p-4 text-sm font-semibold'>채점시 유의사항</h4>
       <ul className='text-sm max-w-[480px] list-disc w-full ml-10 list-inside space-y-2'>
@@ -117,7 +174,7 @@ const ResultsChartAge = () => {
         <li className=''>처음에 흐리게 스케치 한 후 진한 선으로 도형을 완성하는 것은 허용된다.</li>
         <li className=''>검사자가 미처 제지하지 못하는 사이에 수검자가 반응을 수정한 경우 수정 전의 도형이 정답 채점 기준에 명확하게 맞으면 1점을 주고, 만약 불분명하거나 확신하기 어려운 경우 오답으로 처리한다.</li>
       </ul>
-      <hr className='my-2'/>
+      <hr className='my-2' />
       <h4 className='p-4 text-sm font-semibold'>총점 산정 방식</h4>
       <ul className='text-sm max-w-[480px] list-disc w-full ml-10 list-inside space-y-2'>
         <li className=''>VMI 각 문항에 대한 채점이 완성되고 나면 1점 문항의 수르 합산하여 수검자의 VMI 원점수를 산출한다.</li>
