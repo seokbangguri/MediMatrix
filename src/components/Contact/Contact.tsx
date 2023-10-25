eae6bab0ebc2668de163aa4e40357b4232e7c962
Usmon <usmonhakimov1999@gmail.com>
2023-10-25 12:34:37 +0900

    change container border

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,7 +4,7 @@ import { Text, Heading, Button } from "../../components";
 function Contact() {
   return (
     <section className="lg:px-10 mt-20 mb-20 flex items-center w-full lg:max-w-[1445px] mx-auto" id="contact">
-      <div className="w-full bg-white-blue py-20 rounded-[100px] ">
+      <div className="w-full bg-white-blue py-20 rounded-tr-[100px] rounded-bl-[100px] ">
         <Heading tag="h2">Contact Us</Heading>
         <Text size="m" styles="text-center my-3 max-w-[600px] m-auto text-sm md:text-lg">
           Complete the form below to connect with our team of experts on implementing digital primary care solutions.
